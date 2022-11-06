edd0661f5ca99928fe266132ec445b2cb12724bb
bipul <bipulkumar462@gmail.com>
2022-11-06 11:36:22 +0530

    UserLogin

---
+++ Wondermart/UserLoginApi.java
@@ -0,0 +1,31 @@
+@PostMapping("/E_commerceUserlogin")
+public Map Userlogin(HttpServletRequest req)throws ClassNotFoundException, SQLException
+{
+	Class.forName("com.mysql.jdbc.Driver");
+	Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bipul","root","Bipultk@1");
+	String username=req.getParameter("username");
+	String password=req.getParameter("password");
+	Statement stmt =con.createStatement();
+	String query = "select * from E_commerceUserdetails where username='"+username+"'" ;
+	ResultSet rs =stmt.executeQuery(query);
+	HashMap map= new HashMap();
+	if(rs.next())
+	{
+		if(rs.getString("password").equals(password))
+		{
+			map.put("message","you are successfully login");
+			return map;
+		}
+		else
+		{
+			map.put("message","Wrong password");
+			return map;
+		}
+	}
+	else 
+	{
+		map.put("message","you dont don't have login credentials  first signup  ");
+		return map;
+	}
+}
+
